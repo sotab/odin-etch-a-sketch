d644eb3d7099486a419458450e8b8a1f1fb2b50e
Tamas Szonyi <tamas.szonyi@deepl.com>
2024-11-20 09:58:46 +0100

    add generate button and functionality

--- etchSketch.js
+++ etchSketch.js
@@ -1,4 +1,8 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('#container')
+const body = document.querySelector('body')
+const genGridBtn = document.createElement('button')
+genGridBtn.textContent = 'Generate'
+body.appendChild(genGridBtn)
 
 const getRandomColor = () => {
     const colors = ['blue', 'pink', 'purple', 'yellow', 'blue', 'green', 'brown', 'red']
@@ -6,24 +10,46 @@ const getRandomColor = () => {
     return colors[random]
 }
 
+const getGridSize = () => {
+    const gridSize = window.prompt('Enter a number in the range of 1 - 100')
+    return parseInt(gridSize, 10)
+}
+
+const clearGridContainer = () => {
+    const container = document.getElementById('container')
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
+
 function generateSketchBox(nr = 16) {
     for (let i = 0; i < nr; i++) {
-        const boxColumn = document.createElement('div')
-        boxColumn.className = 'box-column'
+        const gridColumn = document.createElement('div')
+        gridColumn.id = 'grid-column'
         for (let i = 0; i < nr; i++) {
             const height = 500 / nr
             const box = document.createElement('div')
             box.className = 'sketch-box'
             box.setAttribute('style', `height: ${height}px;`)
-            boxColumn.appendChild(box)
+            gridColumn.appendChild(box)
 
             box.addEventListener('mouseover', () => {
                 box.style.backgroundColor = getRandomColor()
             })
         }
-        container.appendChild(boxColumn)
+        container.appendChild(gridColumn)
     }
 }
 
-// console.log(getRandomColor())
-generateSketchBox(100)
+genGridBtn.addEventListener('click', () => {
+    let gridSize = getGridSize()
+    if (gridSize > 0 && gridSize < 101 ) {
+        clearGridContainer()
+        generateSketchBox(gridSize)
+    } else {
+        window.alert('Cannot generate grid! Please click generate again and enter a number in the range of 1 - 100')
+    }
+
+})
+
+generateSketchBox(16)
